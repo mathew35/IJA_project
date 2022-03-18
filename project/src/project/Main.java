5b185a289ad1314144c34cb14df2ff08c1885079
mathew35 <matus549@gmail.com>
2022-03-18 18:28:31 +0100

    Update on gui

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -4,13 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.awt.*;
 
 public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getClassLoader().getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 300);
+        Scene scene = new Scene(fxmlLoader.load(), Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2, Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2);
         stage.setTitle("Projekt - UML diagram handler");
         stage.setScene(scene);
         stage.show();
--- project/src/project/MainController.java
+++ project/src/project/MainController.java
@@ -2,15 +2,28 @@ package project;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import uml.ClassDiagram;
+import javafx.scene.control.TextField;
+import java.awt.*;
 
 public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        ClassDiagram diag = new ClassDiagram("1st diag ever");
-        welcomeText.setText("Welcome to JavaFX Application!" + diag.getName() + " has been created");
+    private TextField UMLname;
+
+    @FXML
+    protected void onCreateUMLButtonClick() {
+        if(UMLname.getText().length() == 0){
+            welcomeText.setText("Wrong input");
+        }
+        else{
+            welcomeText.setText("New Diagram: "+UMLname.getText());
+        }
     }
+
+    @FXML
+    protected void onOpenUMLButtonClick(){
+
+    };
 }
