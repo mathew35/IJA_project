ba268545af3eb31c825d7dba7733b95963348349
JuiceLD <33583594+JuiceLD@users.noreply.github.com>
2022-04-12 16:27:20 +0200

    Documentation Edit

--- project/src/project/EditorController.java
+++ project/src/project/EditorController.java
@@ -1,20 +1,58 @@
 package project;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import java.util.ResourceBundle;
+import java.net.URL;
+import javafx.fxml.*;
+import javafx.scene.control.*;
 
-public class EditorController {
+import uml.*;
+
+public class EditorController implements Initializable{
+    @FXML
+    private TreeView<String> ClassTree;
+
+    @FXML
+    private TextField ClassName;
+
     @FXML
     private TabPane tabPane;
 
-    @FXML
-    public void initialize() {
-        Tab deleted = tabPane.getSelectionModel().getSelectedItem();
-        tabPane.getTabs().remove(deleted);
-        tabPane.getSelectionModel().selectLast();    
+    
+    public void SelectClass() {
     }
     
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1){
+        ClassDiagram cd = new ClassDiagram("Diagram");
+        TreeItem<String> rootItem = new TreeItem<>(cd.getName());
+        ClassTree.setShowRoot(false);
+        ClassTree.setRoot(rootItem);
+        ClassName.setPromptText("Class Name");
+
+        Tab deleted = tabPane.getSelectionModel().getSelectedItem();
+        tabPane.getTabs().remove(deleted);
+        tabPane.getSelectionModel().selectLast(); 
+    }
+
+    public void selectItem(){
+        TreeItem<String> item = ClassTree.getSelectionModel().getSelectedItem();
+        if (item != null){
+            ClassName.setText(item.getValue());
+        }
+    }
+    public void onAddClassClick(){
+        TreeItem<String> rootItem = ClassTree.getRoot();
+        String text = ClassName.getText();
+        if (text.isEmpty()) {
+            ClassName.setPromptText("Zadaj nazov novej triedy");
+            ClassName.setStyle("-fx-prompt-text-fill: red");
+            return;
+        }
+        rootItem.getChildren().add(new TreeItem<>(text));
+        ClassName.setText("");
+    }
+    public void onAddSubclassClick(){}
+
     @FXML
     private void addTab() {
         int numTabs = tabPane.getTabs().size();
